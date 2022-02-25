e4fe6fc394ab53e62bcc9ecc49c1245e0a018ab0
spacepolice10 <savemyrights0101@gmail.com>
2022-02-25 15:29:06 +0300

    feat: implement store with numbers

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -1,4 +1,4 @@
-import { Row, Col, Button, Space } from 'antd'
+import { Row, Col, Button } from 'antd'
 import { useDispatch } from 'react-redux'
 import { add } from '../redux/addNumberSlice'
 const style={margin: '5px'}
@@ -9,43 +9,43 @@ const Numbers = () => {
     <div>
         <Row justify='center'>
             <Col span={8}>
-                <Button onClick={() => {dispatch(add('8'))}} style={style} shape='circle'>7</Button>
+                <Button onClick={() => {dispatch(add('7'))}} style={style} shape='circle' size='large' type='primary'>7</Button>
             </Col>
             <Col span={8}>
-                <Button style={style} shape='circle'>8</Button>
+                <Button onClick={() => {dispatch(add('8'))}} style={style} shape='circle' size='large' type='primary'>8</Button>
             </Col>
             <Col span={8}>
-                <Button style={style} shape='circle'>9</Button>
-            </Col>
-        </Row>
-        <Row>
-            <Col span={8}>
-                <Button style={style} shape='circle'>4</Button>
-            </Col>
-            <Col span={8}>
-                <Button style={style} shape='circle'>5</Button>
-            </Col>
-            <Col span={8}>
-                <Button style={style} shape='circle'>6</Button>
+                <Button onClick={() => {dispatch(add('9'))}} style={style} shape='circle' size='large' type='primary'>9</Button>
             </Col>
         </Row>
         <Row>
             <Col span={8}>
-                <Button style={style} shape='circle'>1</Button>
+                <Button onClick={() => {dispatch(add('4'))}} style={style} shape='circle' size='large' type='primary'>4</Button>
             </Col>
             <Col span={8}>
-                <Button style={style} shape='circle'>2</Button>
+                <Button onClick={() => {dispatch(add('5'))}} style={style} shape='circle' size='large' type='primary'>5</Button>
             </Col>
             <Col span={8}>
-                <Button style={style} shape='circle'>3</Button>
+                <Button onClick={() => {dispatch(add('6'))}} style={style} shape='circle' size='large' type='primary'>6</Button>
             </Col>
         </Row>
         <Row>
             <Col span={8}>
-                <Button style={style} shape='circle'>0</Button>
+                <Button onClick={() => {dispatch(add('1'))}} style={style} shape='circle' size='large' type='primary'>1</Button>
             </Col>
             <Col span={8}>
-                <Button style={style} shape='circle'>.</Button>
+                <Button onClick={() => {dispatch(add('2'))}} style={style} shape='circle' size='large' type='primary'>2</Button>
+            </Col>
+            <Col span={8}>
+                <Button onClick={() => {dispatch(add('3'))}} style={style} shape='circle' size='large' type='primary'>3</Button>
+            </Col>
+        </Row>
+        <Row>
+            <Col span={8}>
+                <Button onClick={() => {dispatch(add('0'))}} style={style} shape='circle' size='large' type='primary'>0</Button>
+            </Col>
+            <Col span={8}>
+                <Button onClick={() => {dispatch(add('.'))}} style={style} shape='circle' size='large' type='primary'>.</Button>
             </Col>
         </Row>
     </div>
