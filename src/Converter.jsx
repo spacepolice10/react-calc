f1b32de311b3364869958388442b6ec29a699dc9
spacepolice10 <savemyrights0101@gmail.com>
2022-02-25 19:29:46 +0300

    feat: add switchers to converter component

--- src/Converter.jsx
+++ src/Converter.jsx
@@ -1,5 +1,5 @@
-import { DollarOutlined, ExpandAltOutlined } from "@ant-design/icons"
-import { Button } from "antd"
+import { ClockCircleOutlined, CopyrightOutlined, DollarOutlined, ExpandAltOutlined } from "@ant-design/icons"
+import { Button, Tooltip } from "antd"
 
 const style = {margin: '5px', width: '65px', height: '65px'}
 
@@ -7,8 +7,18 @@ const Converter = () => {
     return (
         <>
             <div>
-                <Button icon={<DollarOutlined />}></Button>
-                <Button icon={<ExpandAltOutlined />}></Button>
+                <Tooltip placement="bottom" title="Convert currencies">
+                    <Button style={style} shape="circle" type="primary" icon={<DollarOutlined style={{fontSize: '30px'}} />}></Button>
+                </Tooltip>
+                <Tooltip placement="bottom" title="Convert distances">
+                    <Button style={style} shape="circle" type="primary" icon={<ExpandAltOutlined style={{fontSize: '30px'}} />}></Button>
+                </Tooltip>
+                <Tooltip placement="bottom" title="Convert degrees">
+                    <Button style={style} shape="circle" type="primary" icon={<CopyrightOutlined style={{fontSize: '30px'}} />}></Button>
+                </Tooltip>
+                <Tooltip placement="bottom" title="Convert time">
+                    <Button style={style} shape="circle" type="primary" icon={<ClockCircleOutlined style={{fontSize: '30px'}} />}></Button>
+                </Tooltip>
             </div>
         </>
     )
